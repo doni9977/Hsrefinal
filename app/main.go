dba94c8cda02395fe863b0fef72c4dbd4f6358ac
Aknur <whitee.ray.haha@gmail.com>
2026-05-16 16:55:24 +0500

    fix: restore light Dockerfile and pipeline configuration after merge

--- app/main.go
+++ app/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-    fmt.Println("SRE Capstone App is successfully running!")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to SRE Capstone App inside Minikube!")
+	})
+
+	fmt.Println("Server is starting on port 8080...")
+	
+	
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
